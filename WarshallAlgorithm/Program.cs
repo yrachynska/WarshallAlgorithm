b1d0fbf0947edbf3aedf5e43f130c95d181aace2
yrachynska <yrachynska@kse.org.ua>
2025-04-10 14:34:46 +0300

    Change main program

--- WarshallAlgorithm/Program.cs
+++ WarshallAlgorithm/Program.cs
@@ -1,4 +1,10 @@
 ﻿
 using WarshallAlgorithm;
 
-GraphGenerator.Print(GraphGenerator.GenerateGraph(10, 20));
+Console.WriteLine("Enter number of vertices: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter density in percents: ");
+int q = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("If you want oriented graph enter '+', if no enter '-' : ");
+char or = Convert.ToChar(Console.ReadLine());
+GraphGenerator.Print(GraphGenerator.GenerateGraph(n, q, or));
