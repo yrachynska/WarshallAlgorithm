2855ff666f2e7d87e9059bc6ede5a234c185b67a
milenayer <myerkovich@kse.org.ua>
2025-04-11 17:13:05 +0300

    warshall algorithm

--- WarshallAlgorithm/Program.cs
+++ WarshallAlgorithm/Program.cs
@@ -7,4 +7,9 @@ Console.WriteLine("Enter density in percents: ");
 int q = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("If you want oriented graph enter '+', if no enter '-' : ");
 char or = Convert.ToChar(Console.ReadLine());
-GraphGenerator.Print(GraphGenerator.GenerateGraph(n, q, or));
+int[,] graph= GraphGenerator.GenerateGraph(n, q, or);
+GraphGenerator.Print(graph);
+
+Console.WriteLine("Transitive closure of a graph:");
+int[,] closure = Warshall.WarshallAlgorithm(graph);
+Warshall.PrintMatrix(closure);
